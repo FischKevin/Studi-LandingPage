d88d1eb041e96ceb0f84c02bbd44a7722b07b347
Kévin Fischer <kevinfischer@live.fr>
2024-03-13 15:12:04 +0100

    Footer.jsx done

--- src/components/Base/Footer.jsx
+++ src/components/Base/Footer.jsx
@@ -1,19 +1,81 @@
-import { Typography, Container, Box } from '@mui/material';
+import React from 'react';
+import { Typography, Box, Grid, Link, IconButton } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter'; // Remplacer par l'icône souhaitée pour "x"
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Footer() {
   return (
-    <Box component="footer" sx={{ bgcolor: 'background.paper', p: 6 }}>
-      <Container maxWidth="lg">
-        {/* Liens et informations ici */}
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          © 2024 Nom de l Event. Tous droits réservés.
-        </Typography>
-      </Container>
+    <Box component="footer" sx={{ bgcolor: 'black', p: 6, color: 'white' }}>
+      <Grid container spacing={4} justifyContent="center">
+        <Grid item xs={12} sm={4} textAlign="center">
+          <Box
+            component="img"
+            src="/public/logo_full.png"
+            alt="Logo de l'événement"
+            sx={{ width: 128, height: 'auto', mb: 2 }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={4} textAlign="center">
+          <Typography gutterBottom>
+            Rejoignez-nous sur les réseaux sociaux
+          </Typography>
+          <Box>
+            <IconButton
+              color="inherit"
+              href="https://facebook.com"
+              target="_blank"
+            >
+              <FacebookIcon />
+            </IconButton>
+            <IconButton
+              color="inherit"
+              href="https://twitter.com"
+              target="_blank"
+            >
+              <TwitterIcon />
+            </IconButton>
+            <IconButton
+              color="inherit"
+              href="https://youtube.com"
+              target="_blank"
+            >
+              <YouTubeIcon />
+            </IconButton>
+            <IconButton
+              color="inherit"
+              href="https://instagram.com"
+              target="_blank"
+            >
+              <InstagramIcon />
+            </IconButton>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4} textAlign="center">
+          <Link
+            href="/tickets"
+            color="inherit"
+            sx={{ display: 'block', mb: 1 }}
+          >
+            Réservation de billets
+          </Link>
+          <Link href="/shop" color="inherit" sx={{ display: 'block', mb: 1 }}>
+            Boutique de goodies
+          </Link>
+          <Typography component="div" sx={{ height: 24 }} />{' '}
+          {/* Espacement vide */}
+          <Link href="/legal" color="inherit" sx={{ display: 'block', mb: 1 }}>
+            Mentions légales
+          </Link>
+          <Link href="/confidentiality" color="inherit">
+            Politique de confidentialité
+          </Link>
+        </Grid>
+      </Grid>
+      <Typography variant="subtitle1" align="center" sx={{ mt: 4 }}>
+        © 2024 Press Start To Fight Cancer. Tous droits réservés.
+      </Typography>
     </Box>
   );
 }
--- src/components/landingPage/Contact.jsx
+++ src/components/landingPage/Contact.jsx
@@ -42,101 +42,129 @@ function Contact() {
         }}
       >
         {/* Formulaire de contact */}
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, maxWidth: '400px' }}>
-          {/* Vos champs TextField ici */}
-          <TextField
-          label="Prénom"
-          variant="outlined"
+        <Box
           sx={{
-            borderRadius: '10px',
-            margin: '0',
-            bgcolor: 'white',
-            '& .MuiOutlinedInput-root': { borderRadius: '10px' },
-          }}
-        />
-        <TextField
-          label="Nom"
-          variant="outlined"
-          sx={{
-            borderRadius: '10px',
-            bgcolor: 'white',
-            '& .MuiOutlinedInput-root': { borderRadius: '10px' },
-          }}
-        />
-        <TextField
-          label="E-mail"
-          variant="outlined"
-          sx={{
-            borderRadius: '10px',
-            bgcolor: 'white',
-            '& .MuiOutlinedInput-root': { borderRadius: '10px' },
-          }}
-        />
-        <TextField
-          label="Message"
-          variant="outlined"
-          multiline
-          rows={4}
-          sx={{
-            borderRadius: '10px',
-            bgcolor: 'white',
-            '& .MuiOutlinedInput-root': { borderRadius: '10px' },
-          }}
-        />
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: '#E84142',
-            color: 'white',
-            border: '1px solid #C01818',
-            borderRadius: '10px',
-            padding: '10px 20px',
-            fontSize: '16px',
-            '&:hover': {
-              backgroundColor: '#C01818',
-            },
-            boxShadow: '0px 6px 8px rgba(0, 0, 0, 0.2)',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            maxWidth: '400px',
           }}
         >
-          ENVOYER
-        </Button>
+          {/* Vos champs TextField ici */}
+          <TextField
+            label="Prénom"
+            variant="outlined"
+            sx={{
+              borderRadius: '10px',
+              margin: '0',
+              bgcolor: 'white',
+              '& .MuiOutlinedInput-root': { borderRadius: '10px' },
+            }}
+          />
+          <TextField
+            label="Nom"
+            variant="outlined"
+            sx={{
+              borderRadius: '10px',
+              bgcolor: 'white',
+              '& .MuiOutlinedInput-root': { borderRadius: '10px' },
+            }}
+          />
+          <TextField
+            label="E-mail"
+            variant="outlined"
+            sx={{
+              borderRadius: '10px',
+              bgcolor: 'white',
+              '& .MuiOutlinedInput-root': { borderRadius: '10px' },
+            }}
+          />
+          <TextField
+            label="Message"
+            variant="outlined"
+            multiline
+            rows={4}
+            sx={{
+              borderRadius: '10px',
+              bgcolor: 'white',
+              '& .MuiOutlinedInput-root': { borderRadius: '10px' },
+            }}
+          />
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: '#E84142',
+              color: 'white',
+              border: '1px solid #C01818',
+              borderRadius: '10px',
+              padding: '10px 20px',
+              fontSize: '16px',
+              '&:hover': {
+                backgroundColor: '#C01818',
+              },
+              boxShadow: '0px 6px 8px rgba(0, 0, 0, 0.2)',
+            }}
+          >
+            ENVOYER
+          </Button>
         </Box>
 
         {/* Section Newsletter */}
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, maxWidth: '400px' }}>
-          <Box sx={{ backgroundColor: 'rgba(255, 255, 255, 0.2)', borderRadius: '10px', padding: '10px 20px', textAlign: 'center' }}>
-            <Typography variant="h6" sx={{ fontFamily: 'Roboto', color: 'white', textShadow:'0px 8px 10px rgba(0, 0, 0, 0.4)' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            maxWidth: '400px',
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: 'rgba(255, 255, 255, 0.2)',
+              borderRadius: '10px',
+              padding: '10px 20px',
+              textAlign: 'center',
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{
+                fontFamily: 'Roboto',
+                color: 'white',
+                textShadow: '0px 8px 10px rgba(0, 0, 0, 0.4)',
+              }}
+            >
               Inscription à la newsletter
             </Typography>
           </Box>
           {/* Champ E-mail et bouton ENVOYER pour la newsletter ici */}
           <TextField
-          label="E-mail"
-          variant="outlined"
-          sx={{
-            margin: '0',
-            borderRadius: '10px',
-            bgcolor: 'white',
-            '& .MuiOutlinedInput-root': { borderRadius: '10px' },
-          }}
-        />
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: '#E84142',
-            color: 'white',
-            border: '1px solid #C01818',
-            borderRadius: '10px',
-            padding: '10px 20px',
-            fontSize: '16px',
-            '&:hover': {
-              backgroundColor: '#C01818',
-            },
-            boxShadow: '0px 6px 8px rgba(0, 0, 0, 0.2)',
-          }}
-        >
-          ENVOYER
-        </Button>
+            label="E-mail"
+            variant="outlined"
+            sx={{
+              margin: '0',
+              borderRadius: '10px',
+              bgcolor: 'white',
+              '& .MuiOutlinedInput-root': { borderRadius: '10px' },
+            }}
+          />
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: '#E84142',
+              color: 'white',
+              border: '1px solid #C01818',
+              borderRadius: '10px',
+              padding: '10px 20px',
+              fontSize: '16px',
+              '&:hover': {
+                backgroundColor: '#C01818',
+              },
+              boxShadow: '0px 6px 8px rgba(0, 0, 0, 0.2)',
+            }}
+          >
+            ENVOYER
+          </Button>
         </Box>
       </Box>
     </Container>
