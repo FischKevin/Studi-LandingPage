7e67765cb513fdaf219190bead12a079a3a1d651
Kévin Fischer <kevinfischer@live.fr>
2024-03-13 09:22:57 +0100

    fixed navbar width because of position property

--- src/components/Base/Navbar.jsx
+++ src/components/Base/Navbar.jsx
@@ -1,42 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { AppBar, Toolbar } from '@mui/material';
-import Box from '@mui/material/Box';
+import { AppBar, Toolbar, Box } from '@mui/material';
 
 function Navbar() {
   const [shrink, setShrink] = useState(false);
 
-  const handleScroll = () => {
-    if (window.scrollY > 100) {
-      setShrink(true);
-    } else {
-      setShrink(false);
-    }
-  };
-
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
+    const handleScroll = () => {
+      setShrink(window.scrollY > 100);
     };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
-    <AppBar className='navbar'
-      position="fixed"
-      maxWidth="1440px"
-      sx={{  transition: '0.3s', ...(shrink && { padding: '10px 0' }) }}
-    >
-      <Toolbar sx={{  marginBottom: '30px', paddingTop: '20px', minHeight: shrink ? '56px' : '64px' }}>
-        <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
-          <img
-            src="/logo_text.png"
-            alt="Logo"
-            style={{ maxWidth: shrink ? '150px' : '250px', height: 'auto' }}
-          />
+    // Box externe pour gérer la fixation et la pleine largeur
+    <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%', position: 'fixed', top: 0, left: 0, zIndex: 1100 }}>
+      {/* AppBar qui agit comme un conteneur pour le contenu centré */}
+      <AppBar sx={{ position: 'sticky', top: 0, zIndex: 1100, backgroundColor: 'transparent', maxWidth: '1440px', margin: '0 auto', width: '100%' }}>
+        {/* Box interne pour limiter la largeur du contenu de la barre */}
+        <Box sx={{ width: '100%', maxWidth: '1440px', margin: '0 auto' }}>
+          <Toolbar sx={{ width: '100%', justifyContent: 'center', padding: shrink ? '10px 0' : '20px 0', minHeight: shrink ? '56px' : '64px' }}>
+            <img src="/logo_text.png" alt="Logo" style={{ maxWidth: shrink ? '150px' : '250px', height: 'auto' }} />
+          </Toolbar>
         </Box>
-      </Toolbar>
-    </AppBar>
+      </AppBar>
+    </Box>
   );
 }
 
