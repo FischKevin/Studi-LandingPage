f3397fac4472047e67777851186de8599c5872e0
Kévin Fischer <kevinfischer@live.fr>
2024-04-05 15:44:08 +0200

    site load optimization

--- src/components/usePageViews.jsx
+++ src/components/usePageViews.jsx
@@ -3,10 +3,15 @@ import { useLocation } from 'react-router-dom';
 
 const UsePageViews = () => {
   let location = useLocation();
+
   useEffect(() => {
-    window.gtag('config', 'G-6KLWYEWW0Y', {
-      page_path: location.pathname,
-    });
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-6KLWYEWW0Y', {
+        page_path: location.pathname,
+      });
+    } else {
+      console.warn('gtag not defined');
+    }
   }, [location]);
 };
 
