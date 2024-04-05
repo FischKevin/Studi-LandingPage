b66828be826fe09f72051ae6c98db31279b02150
Kévin Fischer <kevinfischer@live.fr>
2024-04-05 15:06:47 +0200

    added rgpd links to cookieConsent and center component

--- src/components/CookieConsent.jsx
+++ src/components/CookieConsent.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 const CookieConsent = () => {
   const [showBanner, setShowBanner] = useState(false);
@@ -32,6 +33,9 @@ const CookieConsent = () => {
       sx={{
         position: 'fixed',
         bottom: 0,
+        maxWidth: '1440px',
+        width: '100%',
+        zIndex: 1,
         bgcolor: 'background.paper',
         p: 2,
         display: 'flex',
@@ -42,8 +46,7 @@ const CookieConsent = () => {
       }}
     >
       <Typography>
-        Nous utilisons des cookies pour améliorer votre expérience. En
-        continuant à utiliser ce site, vous acceptez notre politique de cookies.
+        Nous utilisons des cookies pour améliorer votre expérience. En continuant à utiliser ce site, vous acceptez notre <Link href="/cookie" style={{color: 'inherit', textDecoration: 'underline'}}>politique de cookies</Link>, notre <Link href="/legal" style={{color: 'inherit', textDecoration: 'underline'}}>mentions légales</Link> et notre <Link href="/confidentiality" style={{color: 'inherit', textDecoration: 'underline'}}>politique de confidentialité</Link>.
       </Typography>
       <Button
         className="cookie"
